b5627cb474cca576608583868716a78d0207fe2e
ashish Kumar <ashishkumarmjl01@gmail.com>
2025-04-14 17:56:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const music_list = [
     img: "/images/P18.png",
     name: "Mayi_Ri",
     artist: "",
-    music: "/music/Mayi_Ri_-_OST___Audio_🎧___Asrar___Waqar_Ali___ARY_Digital(128k).mp3",
+    music: "./music/Mayi_Ri_-_OST___Audio_🎧___Asrar___Waqar_Ali___ARY_Digital(128k).mp3",
   },
   {
     img: "./images/P1.png",
