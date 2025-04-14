b20722a3ff7db0e4daab98d1baf996a0ee44a0d1
ashish Kumar <ashishkumarmjl01@gmail.com>
2025-04-14 17:03:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let updateTimer;
 
 const music_list = [
   {
-    img: "./images/P18.png",
+    img: "images/P18.png",
     name: "Mayi_Ri",
     artist: "mari ri",
     music: './music/Mayi_Ri_-_OST___Audio_🎧___Asrar___Waqar_Ali___ARY_Digital(128k).mp3',
